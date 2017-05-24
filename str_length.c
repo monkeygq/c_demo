81e81c9fc1f69ac64dd839d86ad0fdfc6bbfb9a8
houguanqun <houguanqun@score.cool>
2017-05-24 11:11:19 +0800

    same as function str_len

---
+++ str_length.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+size_t str_length(const char *str){ 
+  const char *s;
+  for(s=str; *s; ++s){}
+  return s-str;
+}
+
+int main(void) { 
+  const char *str ="qwer";
+  printf("%lu\n", str_length(str));
+  return 0;
+}
+
+// size_t means unsigned long
