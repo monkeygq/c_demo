e7311a28415f106d5da594076a61b83dd46a9f04
houguanqun <houguanqun@score.cool>
2017-05-24 13:44:15 +0800

    implement strcpy

---
+++ str_copy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+char* str_copy(char *to, const char *from) {
+  char *p = to;
+  while((*p++ = *from++) != '\0');
+  return to;
+}
+int main(void) {
+  const char *from = "qwer";
+  char to[5];
+  str_copy(to, from);
+  printf("%s\n", to);
+  return 0;
+}
+
