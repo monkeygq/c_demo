4b32f0e033ac3ee60faa1db81f5d542f9e5672f4
hgq6389465@gmail.com <hgq6389465@gmail.com>
2017-12-15 16:08:54 +0800

    update

--- project/cpuid.c
+++ project/cpuid.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-  int regs[4];
-  __asm__("movl $0x0b, %eax");
+  unsigned int regs[4];
   __asm__(
-           "cpuid"
-           :"=a"(regs[0]), "=b"(regs[1]), "=c"(regs[2]), "=d"(regs[3])
+		   "movl $0xa, %%eax\n\t"
+		   "cpuid\n\t"
+		   :"=a"(regs[0]), "=b"(regs[1]), "=c"(regs[2]), "=d"(regs[3])
       );
 
-  printf("regs[0]=%d,regs[1]=%d,regs[2]=%d,regs[3]=%d\n", regs[0], regs[1], regs[2], regs[3]);
+  printf("regs[0]=%u,regs[1]=%u,regs[2]=%u,regs[3]=%u\n", regs[0], regs[1], regs[2], regs[3]);
   return 0;
 }
