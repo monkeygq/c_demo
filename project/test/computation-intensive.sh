76d21b13388144af882ae4ac0b15aee3f14a1674
monkeygq <hgq6389465@gmail.com>
2018-01-14 15:15:54 +0800

    more perf record

--- project/test/computation-intensive.sh
+++ project/test/computation-intensive.sh
@@ -1,4 +1,15 @@
 perf record -a -o perf.data.1 &
 perf record -a -o perf.data.2 &
+perf record -a -o perf.data.3 &
+perf record -a -o perf.data.4 &
+perf record -a -o perf.data.5 &
+perf record -a -o perf.data.6 &
+perf record -a -o perf.data.7 &
+perf record -a -o perf.data.8 &
+perf record -a -o perf.data.9 &
+perf record -a -o perf.data.10 &
+perf record -a -o perf.data.11 &
+perf record -a -o perf.data.12 &
+perf record -a -o perf.data.13 &
 sleep 10
 ps aux | grep "perf record" | awk '{print $2}' | xargs kill -SIGINT &
