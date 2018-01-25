a4bb31e56d65b60b98b48438f5f567cd4a41d2e6
monkeygq <hgq6389465@gmail.com>
2018-01-25 21:31:56 +0800

    perf eps

--- project/test/first_experiment/perf.rb
+++ project/test/first_experiment/perf.rb
@@ -1,9 +1,11 @@
+output = File.new("computation-intensive.data.available", "w")
 File.open("computation-intensive.data", "r") do |file|
 	while line = file.gets
 		sample_period = line.split("=").last.strip
 		line = file.gets
 		line = file.gets
 		sample_count = line.split("(").last.split.first
-		puts "#{sample_period} #{sample_count}"
+		output.puts "#{sample_period} #{sample_count}"
 	end
 end
+output.close
