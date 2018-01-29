1dcc60f643da26f07db0b7f532afe73a9a819d5f
monkeygq <hgq6389465@gmail.com>
2018-01-29 12:36:58 +0800

    fifth pm alone data

--- project/test/fifth_experiment/run.sh
+++ project/test/fifth_experiment/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cmd="./memory-intensive >> perf_event.data"
 recompile="gcc -o memory-intensive memory-intensive.c"
+cache="echo 3 > /proc/sys/vm/drop_caches"
 for((size=1;size<=20;size++))
 do
 	change="sed -i '27c #define MAX (1024 \* $size)' memory-intensive.c"
@@ -8,7 +9,8 @@ do
 	eval $recompile 
 	for((i=0;i<20;i++))
 	do
-	sleep 10
+	#sleep 10
+	eval $cache
 	eval $cmd
 	done
 done
